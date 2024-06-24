f4cc53967e522b7ce1269c22b46c3e1a988e1a05
florencia.balestena <ebentancor341@gmail.com>
2024-06-24 17:35:52 -0300

    v2
    v2

---
+++ Js/menu-hamburguesa.js
@@ -0,0 +1,12 @@
+let menu = document.getElementById('menu-h');
+let containerLinks = document.querySelector('.nav-bar');
+
+
+menu.addEventListener('click', function(){
+
+
+containerLinks.classList.toggle('nav-bar')
+containerLinks.classList.toggle('nav-bar-active')
+
+    
+});
