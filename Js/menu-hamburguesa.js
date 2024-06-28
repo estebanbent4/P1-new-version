56f4b536073b3a8089e627685e01605326356b08
florencia.balestena <ebentancor341@gmail.com>
2024-06-28 10:30:59 -0300

    v1.0v.10
    v1.0

--- Js/menu-hamburguesa.js
+++ Js/menu-hamburguesa.js
@@ -1,5 +1,12 @@
-let menu = document.getElementById('menu-h');
+
+
+
+
+let menu = document.getElementById('hamburger');
 let containerLinks = document.querySelector('.nav-bar');
+const hamburgerInput = document.querySelector('.hamburger input');
+const links = document.querySelectorAll('.header-link');
+
 
 
 menu.addEventListener('click', function(){
@@ -8,5 +15,15 @@ menu.addEventListener('click', function(){
 containerLinks.classList.toggle('nav-bar')
 containerLinks.classList.toggle('nav-bar-active')
 
+                                                   
     
+});
+
+
+links.forEach(link => {
+  link.addEventListener('click', () => {
+    containerLinks.classList.remove('nav-bar-active');
+    containerLinks.classList.add('nav-bar');
+    hamburgerInput.checked = false; // Deseleccionamos el input checkbox
+  });
 });
